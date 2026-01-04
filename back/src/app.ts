6d055d602f2c4ef40d18a6c38b18d657cbc5a316
Gabriel Davidenko <gabriel.davidenko@gmail.com>
2026-01-04 21:06:17 +0100

    lint

--- back/src/app.ts
+++ back/src/app.ts
@@ -5,7 +5,8 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.get('/health-check',(req ,res)=> { res.send('server up')});
-
+app.get('/health-check', (req, res) => {
+	res.send('server up');
+});
 
 export default app;
